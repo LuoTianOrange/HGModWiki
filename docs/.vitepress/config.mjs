70b5154368d3e55e2ad0cc68174fba37ccf9a43e
LuoTianOrange <2977277015@qq.com>
2024-01-05 23:05:30 +0800

    更新

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: '主页', link: '/' },
-      { text: '开始', link: '/什么是MOD' }
+      { text: '开始', link: '/什么是MOD' },
     ],
 
     sidebar: [
@@ -17,6 +17,7 @@ export default defineConfig({
         text: '介绍',
         items: [
           { text: '什么是MOD', link: '/什么是MOD' },
+          { text: '快速入门', link: '/快速入门' }
         ]
       }
     ],
@@ -26,7 +27,7 @@ export default defineConfig({
       {
         icon: {
           svg: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 200 200"><defs><style>.cls-1 {fill: #67676c;fill-rule: evenodd;}</style></defs><path id="形状_316_1" data-name="形状 316 1" class="cls-1" d="M71.429,3L179.592,19.108,0,190.926ZM186.735,23.4Q193.367,75.481,200,127.568L9.184,192l6.122-7.517Z" /></svg>',
-        }, 
+        },
         link: 'https://hgadventure.huijiwiki.com/wiki/',
         ariaLabel: 'cool link'
       }
