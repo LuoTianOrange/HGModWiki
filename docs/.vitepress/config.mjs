38f69cf1d682a82c870b77c9494f5373d47553b4
RaitoOrange <2977277015@qq.com>
2024-01-05 18:53:50 +0800

    first commit

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "VitePressTest",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '开始', link: '/什么是MOD' }
+    ],
+
+    sidebar: [
+      {
+        text: '介绍',
+        items: [
+          { text: '什么是MOD', link: '/什么是MOD' },
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      {
+        icon: {
+          svg: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 200 200"><defs><style>.cls-1 {fill: #67676c;fill-rule: evenodd;}</style></defs><path id="形状_316_1" data-name="形状 316 1" class="cls-1" d="M71.429,3L179.592,19.108,0,190.926ZM186.735,23.4Q193.367,75.481,200,127.568L9.184,192l6.122-7.517Z" /></svg>',
+        }, 
+        link: 'https://hgadventure.huijiwiki.com/wiki/',
+        ariaLabel: 'cool link'
+      }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "皓际大冒险"
+  text: "MOD编辑教程"
+  tagline: power by 灯火橘
+  actions:
+    - theme: brand
+      text: 开始
+      link: /什么是MOD
+    # - theme: alt
+    #   text: API Examples
+    #   link: /api-examples
+
+features:
+  - title: 土豆
+    details: 一个土豆，能干什么呢
+  - title: 西瓜
+    details: 西瓜会越吃越饿
+  - title: 草莓
+    details: 草莓，不是菠萝
+---
+![土豆](./image/tudou.png)
+![草莓](./image/草莓.png)
+
