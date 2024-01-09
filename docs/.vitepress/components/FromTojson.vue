afe7994c2eeb436c076774b42127d3f40a8e30cb
灰白草 <41500176+HBcao233@users.noreply.github.com>
2024-01-09 20:59:20 +0800

    Update FromTojson.vue

--- docs/.vitepress/components/FromTojson.vue
+++ docs/.vitepress/components/FromTojson.vue
@@ -74,7 +74,7 @@ import { DocumentCopy,Plus } from '@element-plus/icons-vue'
 const CM_mat1 = ref('')
 const CM_mat1_loading = ref(false)
 const CM_mat1_options = ref([])
-const remoteMethod = (query: string) => {
+const remoteMethod = (query) => {
   if (query) {
     CM_mat1_loading.value = true
     setTimeout(() => {
