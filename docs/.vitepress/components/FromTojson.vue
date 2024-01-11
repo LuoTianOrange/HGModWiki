ce4b088d0104e66d7ba1a8a1eb6a9fca93d4a850
灰白草 <41500176+HBcao233@users.noreply.github.com>
2024-01-11 23:17:11 +0800

    添加光照颜色选择器 (#6)
    * Update README.md
    
    * Update index.md
    
    * Update config.mjs
    
    * Update and rename docs/小工具/JSON快速制作器/CM.md to docs/小工具/JSON快速制作器.md
    
    * Delete docs/小工具/JSON快速制作器/WSITEM.md
    
    * Update config.mjs
    
    * Update JSON快速制作器.md
    
    * Update FromTojson.vue
    
    * Update JSON快速制作器.md
    
    * Update JSON快速制作器.md
    
    * Update FromTojson.vue
    
    * Update config.mjs
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Delete docs/小工具/JSON快速制作器/WSAMMO.md
    
    * Update JSON快速制作器.md
    
    * Update config.mjs
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Create requests.ts
    
    * Update package.json
    
    * Create mediawiki.ts
    
    * Create util.ts
    
    * Update mediawiki.ts
    
    * Update FromTojson.vue
    
    * Update package.json
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Delete docs/.vitepress/components/mediawiki.ts
    
    * Delete docs/.vitepress/components/util.ts
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Delete .vitepress/cache/deps directory
    
    * Delete docs/.vitepress/cache/deps directory
    
    * Update FromTojson.vue
    
    * 选择物品选项中显示id
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * Update FromTojson.vue
    
    * 小修改
    
    * 小修改2
    
    * 修改单选样式
    
    * 单选数据修复
    
    * 添加WSITEM参数
    
    * 修复新增参数类型
    
    * 再次修复WSITEM参数类型
    
    * 修复WSITEM参数-3
    
    * 为选择物品option添加相似度排序
    
    * 添加option相似度排序-2
    
    * 添加option相似度排序-3
    
    * 添加option相似度排序-4
    
    * 添加option相似度排序-5
    
    * 添加相似度排序-6
    
    * 添加相似度排序-7
    
    * 添加颜色选择器
    
    * 添加颜色选择器-2
    
    * 添加颜色选择器-3
    
    * 添加颜色选择器-4
    
    * 添加颜色选择器-5
    
    * 添加颜色选择器-6
    
    ---------
    
    Co-authored-by: RaitoOrange <49292532+LuoTianOrange@users.noreply.github.com>

--- docs/.vitepress/components/FromTojson.vue
+++ docs/.vitepress/components/FromTojson.vue
@@ -139,13 +139,14 @@
                         </el-form-item>
                         <el-form-item class="labeldiv el-from-item">
                             <el-label for="from">光照颜色</el-label>
-                            <el-input class="input-1" v-model.number="WSITEM_Parameter.LightColor" placeholder="LightColor"
-                                oninput="this.value = this.value.replace(/[^0-9]/g, '');" @input="generateOutput"
-                                clearable maxlength="10" type="text" show-word-limit />
+                            <el-input class="input-1" v-model="WSITEM_Parameter.LightColor" placeholder="LightColor"
+                                @input="generateOutput"
+                                clearable maxlength="9" type="text" show-word-limit />
+                            <client-only><el-color-picker v-model="WSITEM_Parameter.LightColor" show-alpha color-format="hex" :predefine="['#ff4b4b','#55f5ff', '#ffff00', '#ff7c81', '#fb6e92', '#7c9eff', '#81fe7c', '#ffaa4b', '#008000', '#006da0', '#7b92ff', '#eda4ff', '#fea500', '#ff7061', '#fffe84', '#916df6', '#93c0ff', '#93fdff', '#93ffc3', '#c3c3c3', '#FFFAFA']" @change="generateOutput" /></client-only>
                         </el-form-item>
                         <el-form-item class="labeldiv el-from-item">
                             <el-label for="from">光照范围</el-label>
-                            <el-input class="input-1" v-model.number="WSITEM_Parameter.LightRange" placeholder="LightColor"
+                            <el-input class="input-1" v-model.number="WSITEM_Parameter.LightRange" placeholder="LightRange"
                                 oninput="this.value = this.value.replace(/[^0-9]/g, '');" @input="generateOutput"
                                 clearable maxlength="10" type="text" show-word-limit />
                         </el-form-item>
@@ -678,6 +679,9 @@ const generateOutput = () => {
                         } else {
                             return '只能是正整数';
                         }
+                    }
+                    if (k === 'LightColor') {
+                      return v.replace('#', '')
                     }
                     return toUnicode(v);
                 }
