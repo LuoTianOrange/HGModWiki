b5a38fae91d1008ac6a3d54c533c15f118868892
RaitoOrange <2977277015@qq.com>
2024-01-17 21:53:01 +0800

    修改FposX,FposY的文本描述

--- docs/.vitepress/components/FromTojson.vue
+++ docs/.vitepress/components/FromTojson.vue
@@ -28,7 +28,7 @@
                         <el-input class="input-1" v-model="WSITEM_Parameter[i[1]]" :placeholder="i[1]"
                             @input="generateOutput" clearable type="text" />
                     </el-form-item>
-                    <el-form-item class="labeldiv" v-for="i in [['攻击力(饱食度)','atk'],['贴图大小', 'Size'],['贴图位置X', 'FposX'],['贴图位置Y', 'FposY'],['最大堆叠数量', 'maxNum'],['物品价格', 'price']]">
+                    <el-form-item class="labeldiv" v-for="i in [['攻击力(饱食度)','atk'],['贴图大小', 'Size'],['武器弹幕贴图位置X', 'FposX'],['武器弹幕贴图位置Y', 'FposY'],['最大堆叠数量', 'maxNum'],['物品价格', 'price']]">
                         <el-label for="from">{{ i[0] }}</el-label>
                         <el-input class="input-1" v-model.number="WSITEM_Parameter[i[1]]" :placeholder="i[1]"
                             @input="generateOutput" clearable type="text" oninput="this.value = this.value.replace(/[^0-9]/g, '');" />
--- docs/小工具/JSON快速制作器.md
+++ docs/小工具/JSON快速制作器.md
@@ -2,5 +2,6 @@
 import FromToJson from '/.vitepress/components/FromToJson.vue';
 </script>-->
 ### JSON快速制作器
+部分物品使用了查询，但数据库还没有完善，会出现查询不到对应id物品的情况，直接根据物品ID表填入即可
 <FromTojson />
 
